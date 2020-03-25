95e830156993d8113d70bf1fb03cdbfdc254ce66
Melanie Tran <melaniexx02@gmail.com>
2020-03-25 09:05:28 -0600

    Passed it has a category test

--- test/product_test.rb
+++ test/product_test.rb
@@ -5,11 +5,12 @@ require './lib/product'
 class ProductTest < Minitest::Test
   def test_it_exists
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
     assert_instance_of Product, product
   end
 
   def test_it_has_a_category
-    skip
+    
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
